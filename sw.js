e752c84f25ad2c7e3fa23b2fda7feea97a4b849c
gruntins <ritvars.gruntins@gmail.com>
2026-09-10 17:50:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gruntinsfit-v8-1';
+const CACHE='gruntinsfit-v8-0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
