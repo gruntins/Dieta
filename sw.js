83fbe5fb9a97c7b27c92517a978ea4b07d00bf13
gruntins <ritvars.gruntins@gmail.com>
2026-09-10 03:19:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE='dieta-v3';
+const CACHE='dieta-v4';
 const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate',e=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
       .then(()=>self.clients.claim())
   );
 });
-
 self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
   if(e.request.mode==='navigate' || url.pathname.endsWith('/index.html')){
