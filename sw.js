798f0effd383630c6901640dfa729dc5b3b0b102
gruntins <ritvars.gruntins@gmail.com>
2026-09-10 03:08:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-const CACHE='dieta-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => {
+const CACHE='dieta-v3';
+const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(e.request.mode==='navigate' || url.pathname.endsWith('/index.html')){
+    e.respondWith(
+      fetch(e.request).then(r=>{
+        const copy=r.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return r;
+      }).catch(()=>caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
